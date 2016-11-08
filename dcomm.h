766816fc95e3dcb4cb528fb6aa82cfa39c0ae459
luthkur <13514102@std.stei.itb.ac.id>
2016-11-08 22:57:47 +0700

    Fix dcomm.h

--- dcomm.h
+++ dcomm.h
@@ -1,51 +1,50 @@
 /*
-* File : dcomm.h
+  File : dcomm.h
 */
-#ifndef ​ _DCOMM_H_
-#define ​ _DCOMM_H_
+#ifndef _DCOMM_H_
+#define _DCOMM_H_
 
 /* ASCII Const */
-#define ​ SOH 1 ​/* Start of Header Character */
-#define ​ STX 2 ​/* Start of Text Character */
-#define ​ ETX 3 ​/* End of Text Character */
-#define ​ ENQ 5 ​/* Enquiry Character */
-#define ​ ACK 6 ​/* Acknowledgement */
-#define ​ BEL 7 ​/* Message Error Warning */
-#define ​ CR 13 ​/* Carriage Return */
-#define ​ LF 10 ​/* Line Feed */
-#define ​ NAK 21 ​/* Negative Acknowledgement */
-#define ​ Endfile 26 ​/* End of file character */
-#define ​ESC 27 ​/* ESC key */
+#define SOH 1 /* Start of Header Character */
+#define STX 2 /* Start of Text Character */
+#define ETX 3 /* End of Text Character */
+#define ENQ 5 /* Acknowledgement */
+#define BEL 7 /* Message Error Warning */
+#define CR 13 /* Carriage Return */
+#define LF 10 /* Line Feed */
+#define NAK 21 /* Negative Acknowledgement */
+#define Endfile 26 /* End of file Character */
+#define ESC 27 /* ESC key */
 
-/* XON/XOFF protocol */
-#define ​ XON (0x11)
-#define ​ XOFF (0x13)
+/* XON/XOFF Protocol */
+#define XON (0x11)
+#define XOFF (0x13)
 
 /* Const */
-#define ​ BYTESIZE 256 ​/* The maximum value of a byte */
-#define ​ MAXLEN 1024 ​/* Maximum messages length */
+#define BYTESIZE 256 /* THe maximum value of a byte */
+#define MAXLEN 1025 /* Maximum messages length */
 
-typedef enum ​ { ​false ​=0, ​true ​} Boolean;
+typedef enum { false = 0, true } Boolean;
 
-typedef unsigned ​ ​char ​Byte;
+typedef unsigned char Byte;
 
-typedef struct ​QTYPE
+typedef struct QTYPE
 {
-	​unsigned int ​count;
-	​unsigned int ​front;
-	​unsigned int ​rear;
-	​unsigned int ​ maxsize;
-	Byte *data;
+  unsigned int count;
+  unsigned int front;
+  unsigned int rear;
+  unsigned int maxsize;
+  Byte *data;
 } QTYPE;
 
-typedef struct ​MESGB
+typedef struct MESGB
 {
-	​unsigned int ​ soh;
-	​unsigned int ​ stx;
-	​unsigned int ​ etx;
-	Byte checksum;
-	Byte msgno;
-	Byte *data;
-} MESGB;
+  unsigned int soh;
+  unsigned int stx;
+  unsigned int etx;
+  Byte checksum;
+  Byte msgno;
+  Byte *data;
+} MESGB
 
 #endif
