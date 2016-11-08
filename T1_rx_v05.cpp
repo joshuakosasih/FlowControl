bcf464fef5176c5343f98db183b9685b510bf4fe
joshuakosasih <joshua.kosasih@gmail.com>
2016-11-08 23:04:51 +0700

    add few early fix & todo for rcvchar

--- T1_rx_v05.cpp
+++ T1_rx_v05.cpp
@@ -1,11 +1,14 @@
 /*
 * File : T1_rx.cpp
+* Author : Joshua K - 012, Luthfi K, Albertus K - 100
 */
-#include<stdio.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 #include "dcomm.h"
 
@@ -16,7 +19,7 @@
 #define RXQSIZE 8
 
 Byte rxbuf[RXQSIZE];
-QTYPE rcvq = { 0, 0, 0, RXQSIZE, rxbuf };
+QTYPE rcvq = {0, 0, 0, RXQSIZE, rxbuf };
 QTYPE *rxq = &rcvq;
 Byte sent_xonxoff = XON;
 Boolean send_xon = false, send_xoff = false;
@@ -30,13 +33,13 @@ static Byte *q_get(QTYPE *, Byte *);
 
 int main(int argc, char *argv[])
 {
-	struct sockaddr_in server, client;
+	struct sockaddr_in sserver, sclient;
 
 	Byte c;
 	/*
 	Insert code here to bind socket to the port number given in argv[1].
 	*/
-	sockfd = socket(AF_INET, SOCKDGRAM, 0);
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd < 0)
     {
         puts("Could not create socket");
@@ -44,11 +47,11 @@ int main(int argc, char *argv[])
     }
     puts("Socket created");
     
-	server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons(atoi(argv[1]));
+	sserver.sin_family = AF_INET;
+    sserver.sin_addr.s_addr = INADDR_ANY;
+	sserver.sin_port = htons(atoi(argv[1]));
 	
-	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+	if (bind(sockfd,(struct sockaddr *)&sserver, sizeof(sserver)) < 0)
     {
         perror("Bind failed. Error");
         return 1;
@@ -60,38 +63,59 @@ int main(int argc, char *argv[])
 	/* Create child process */
 	
 	/*** IF PARENT PROCESS ***/
-		while ​( ​true ​) {
-			c = *(rcvchar(sockfd, rxq));
+	while (true) {
+		c = *(rcvchar(sockfd, rxq));
 			
-			/* Quit on end of file */
-			​if ​(c == Endfile) {
-				exit(0);
-			}
+		/* Quit on end of file */
+		if (c == Endfile) {
+		exit(0);
 		}
-	​ /*** ELSE IF CHILD PROCESS ***/ ​
-	while ​( ​true ​) {
+	}
+	
+	/*** ELSE IF CHILD PROCESS ***/
+	while (true) {
 	/* Call q_get */
 	/* Can introduce some delay here. */
 	}
 }
 
-static ​Byte *rcvchar( ​int ​ sockfd, QTYPE *queue)
+static Byte *rcvchar(int sockfd, QTYPE *queue)
 {
-		/*
-		Insert code here. Read a character from socket and put it to the receive buffer. If the number of characters in the receive buffer is above certain level, then send XOFF and set a flag (why?). Return a pointer to the buffer where data is put.
-		*/
+	/*
+	Insert code here. Read a character from socket and put it to the receive buffer. If the number of characters in the 
+	* receive buffer is above certain level, then send XOFF and set a flag (why?). Return a pointer to the buffer where data is put.
+	*/
+	if (queue->count >= RXQSIZE) { //buffer full
+		
+		if(!send_xoff) { //XOFF not sent yet
+		sent_xonxoff = XOFF;
+		send_xon = false;
+		
+		//TODO: send 'sent_xonxoff' via socket
+		
+		//if send succeed
+		send_xoff = true;
+		
+		}
+	}
+	else {
+		
+		//TODO: read char from socket (receive)
+		
+	}
 }
 
 /* q_get returns a pointer to the buffer where data is read or NULL if
 * buffer is empty.
 */
 
-static ​Byte *q_get(QTYPE *queue, Byte *data)
+static Byte *q_get(QTYPE *queue, Byte *data)
 {
 	Byte *current;
-	​ /* Nothing in the queue */ ​if ​(!queue->count) ​return ​(NULL);
-
+	/* Nothing in the queue */
+	if (!queue->count) return (NULL);
 	/*
-	Insert code here. Retrieve data from buffer, save it to "current" and "data" If the number of characters in the receive buffer is below certain level, then send XON. Increment front index and check for wraparound.
+	Insert code here. Retrieve data from buffer, save it to "current" and "data" If the number of characters in the receive buffer 
+	* is below certain level, then send XON. Increment front index and check for wraparound.
 	*/
 }
